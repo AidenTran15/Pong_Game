e17c7faaf644a29ebb29843c65c384ffb39ffe4d
https://github.com/AidenTran15 <aidenkiettran@gmail.com>
2022-11-17 11:25:06 +1100

    Created time frame for a game

--- solution.py
+++ solution.py
@@ -5,11 +5,16 @@ WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong Game")
 
+FPS  = 60
 
 def main():
     run = True 
+    clock = pygame.time.Clock()
+
 
     while run:
+        clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
